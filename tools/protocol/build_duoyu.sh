eb19cf70c9839cab08e0f0b55220423f9a3973bf
LittleKey <LittleKeyRain@gmail.com>
2016-12-19 21:35:06 +0800

    update proto for android

--- tools/protocol/build_duoyu.sh
+++ tools/protocol/build_duoyu.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname $0 )" && pwd )"
 GIT_ROOT="$(git rev-parse --show-toplevel)"
 
 cd $SCRIPT_DIR
-sh build_proto.sh $GIT_ROOT $GIT_ROOT/app/duoyu/src/main/java *.proto
-sh build_proto.sh $GIT_ROOT $GIT_ROOT/app/duoyu/src/main/java **/*.proto
+sh build_proto.sh $GIT_ROOT/proto $GIT_ROOT/app/sparkle/src/main/java models/proto/*.proto
+sh build_proto.sh $GIT_ROOT/proto $GIT_ROOT/app/sparkle/src/main/java models/proto/**/*.proto
